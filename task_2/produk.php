38fdbe837d20e8b5d69566edf48ead279167bedb
aldigunawan-dev <78894332+aldigunawan-dev@users.noreply.github.com>
2022-11-04 21:57:02 +0700

    fix

--- task_2/produk.php
+++ task_2/produk.php
@@ -1,15 +1,4 @@
 <?php
   $cari = $_GET['cari'];
-  
-  switch($cari){
-    case "parka":
-        header("Location: /produk/parka.html?cari=$cari");
-        break;
-    case "fleece":
-        header("Location: /produk/fleece.html?cari=$cari");
-        break;
-    case "katun":
-        header("Location: /produk/katun.html?cari=$cari");
-        break;       
-    }
+  header("Location: /produk/katun.html?cari=$cari");    
 ?>
